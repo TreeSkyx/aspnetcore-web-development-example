aff803360b9c9e61a40cdf7bb9bdad2c84c6c923
Wiroon Somphaothong <88649190+TreeSkyx@users.noreply.github.com>
2025-01-28 16:15:06 +0700

    init dotnet project

--- obj/Debug/net9.0/aspnetcore-web-development-example.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspnetcore-web-development-example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcore-web-development-example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8629f3631ec6be874e4fa3dd202a594baa638159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c2d421e47cc6a4cae0a2f29e4f14112416092d")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcore-web-development-example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcore-web-development-example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
